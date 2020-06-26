d4e2afbf0908736388e1530dd0235c1a5823bfcd
Vicarious76 <matheusyanmonteiro@gmail.com>
2020-06-26 16:26:37 -0300

    aplicaçao funcional sem pesquisar

--- src/database/db.js
+++ src/database/db.js
@@ -67,7 +67,7 @@ module.exports = db
 
 //EXCLUIR-----------------------------------------------------------------------------
 
-// db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
+//db.run(`DELETE FROM places WHERE id = ?`, [3], function(err){
 //    if(err) {
 //        return console.log(err)
 //    }
--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const db = require("./database/db.js")
 
 server.use(express.static("public"))
 
+//habilitando o uso do req body
+server.use(express.urlencoded({extended: true}))
+
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views",{
     express: server,
@@ -19,7 +22,50 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+    //console.log(req.query)
+
     return res.render("create-point.html")
+
+})
+
+server.post("/savepoint", (req, res) => {
+
+    //console.log(req.body)
+
+    //inserindo dados no banco de dados.
+    const query = `
+           INSERT INTO places(
+              image,
+              name,
+              address,
+              address2,
+              state,
+              city,
+              items
+            ) VALUES(?,?,?,?,?,?,?);`
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+           ]
+    
+    function afterInsertData(err){
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastro bem sucedido")
+        console.log(this)
+     return res.render("create-point.html", {saved: true})
+
+    }
+    
+    
+     db.run(query, values, afterInsertData)
 
 })
 
@@ -31,9 +77,11 @@ server.get("/search", (req, res) => {
             return console.log(err)
         }
 
+        const total =rows.length
+
         console.log("dados resgastados com sucessoS")
         //mostrar a pagina html com o banco de dados
-        return res.render("search-results.html",{places: rows})
+        return res.render("search-results.html",{places: rows, total: total})
     })
 
 
